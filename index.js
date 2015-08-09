4f16b407ed6540f919e56da5724b909c40246402
RobinQu <robinqu@gmail.com>
2015-08-09 17:01:38 +0800

    fs handler

--- index.js
+++ index.js
@@ -1,17 +1,4 @@
-var path = require('path');
-var fs = require('fs');
-var _ = require('lodash');
-
 var ctx = require('./lib/context');
-
-_.forEach(fs.readdirSync(path.join(__dirname, 'lib', 'plugins')), function (name) {
-  var m = name.match(/^(.+)\.js$/);
-  if(m && m.length === 2) {
-    var Plugin = require('./lib/plugins/' + m[1]);
-    ctx.register(Plugin);
-  }
-});
-
 
 module.exports = function (options) {//logger factory
   return ctx.logger(options);
